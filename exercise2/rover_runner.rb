9f0882fd4cef6aa9d4231097a0a3dee42a5b4d83
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-03 20:33:07 -0400

    debugged main routine of Rover program

--- exercise2/rover_runner.rb
+++ exercise2/rover_runner.rb
@@ -1,17 +1,37 @@
-r = Rover.new(3,4,'N')
+require 'rover'
 
 def main
+  rovers = []
+
+  # "Your program should accept 5 inputs with gets." 
   puts "Please enter the size of the grid"
-  input <<  gets.chomp
-  puts "Please enter the initial position and direction of a rover"
-  r1 = Rover.new(x,y,heading)
-  input << gets.chomp
-  puts "Please enter a series of move and turn instructions for the first rover." 
-  input << gets.chomp
+  input = gets.chomp
+  x,y = input.split(" ")
+  # game.set_grid_size
+
+  puts "Please enter the initial start position and header of rover #1."
+  input = gets.chomp
+  x,y,v = input.split(" ")
+  r = Rover.new(x.to_i, y.to_i, v)
+
+  puts "Please enter the move and turn instructions for rover #1."
+  input = gets.chomp
+  r.set_instructions(input)
+  rovers << r 
+  
   puts "Please enter the initial start position and header of rover #2."
-  input << gets.chomp
+  input = gets.chomp
+  x,y,v = input.split(" ")
+  r = Rover.new(x.to_i, y.to_i, v)
+
   puts "Please enter the move and turn instructions for rover #2."
-  input << gets.chomp                                 
+  input = gets.chomp                                 
+  r.set_instructions(input)
+  rovers << r 
+
+  rovers.each do |r|
+    puts r.run
+  end
 end
 
 main
