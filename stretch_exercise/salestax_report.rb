82d0206118be21a1071f835b3b1a2272a0e131e6
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-05 14:35:08 -0400

    removed some whitespace; used require_relative

--- stretch_exercise/salestax_report.rb
+++ stretch_exercise/salestax_report.rb
@@ -1,21 +1,16 @@
-require 'salestax'
-
-
+require_relative 'salestax'
 
 puts"INPUT:\n\nOutput 1:\n\n"
-
 
 s = Salestax.new 
 s.read_input("input1.dat")
 x=  s.report
 puts "#{x}\n\n"
 
-
 s = Salestax.new 
 s.read_input("input2.dat")
 x=  s.report
 puts "#{x}\n\n"
-
 
 s = Salestax.new 
 s.read_input("input3.dat")
